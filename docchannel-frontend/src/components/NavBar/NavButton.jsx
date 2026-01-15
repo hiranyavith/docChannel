5488fad7433a5fe112ed7824de348fd8a9094e45
HIRANAYE VITHANAGE <hiranyevithange916@gmail.com>
2026-01-15 15:57:08 +0530

    done react routes for login

--- docchannel-frontend/src/components/NavBar/NavButton.jsx
+++ docchannel-frontend/src/components/NavBar/NavButton.jsx
@@ -1,7 +1,7 @@
-function NavButton({ text }) {
+function NavButton({ text, OnClick }) {
   return (
     <>
-      <button className="bg-white text-[#000000] px-3 py-1 md:px-5 md:py-2 md:overflow-hidden rounded-full font-medium hover:bg-gray-100 transition font-nunito font-semibold">
+      <button onClick={OnClick} className="bg-white text-[#000000] px-3 py-1 md:px-5 md:py-2 md:overflow-hidden rounded-full font-medium hover:bg-gray-100 transition font-nunito font-semibold">
         {text}
       </button>
     </>
